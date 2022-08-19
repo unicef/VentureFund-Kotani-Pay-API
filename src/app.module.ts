44baf7c48790153124185c47448732d5b2e2f4fa
Ejleone <Ejgithinji@gmail.com>
2022-08-19 14:43:55 +0300

    add load configs from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoreModule } from '@kotanicore/services/core.module';
 import { AuthModule } from '@kotanicore/auth';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost:27017'),
     CoreModule,
     AuthModule,
