6812509dcf40166ea2ddb94bb78d8f80b803cd18
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2022-08-24 14:28:58 +0300

    :sparkles: added name and phone to login response

--- libs/repository/src/interface/transaction-document.interface.ts
+++ libs/repository/src/interface/transaction-document.interface.ts
@@ -2,10 +2,10 @@ import { Document, ObjectId } from 'mongoose';
 
 export interface TransactionDocumentInterface extends Document {
   _id: ObjectId;
-  date: Date,
-  amount: number,
-  txHash: string,
-  description:string,
-  status:string,
-  type:string,
+  date: Date;
+  amount: number;
+  txHash: string;
+  description: string;
+  status: string;
+  type: string;
 }
